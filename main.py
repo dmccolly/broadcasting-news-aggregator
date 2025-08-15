fa6dee2a83b7cc6d92fdad73c00174761086440e
dmccolly <128931847+dmccolly@users.noreply.github.com>
2025-08-15 17:59:21 +0000

    Create main.py
    Initial Flask application for broadcasting news aggregator with health check endpoint

---
+++ main.py
@@ -0,0 +1,31 @@
+o',f r'otmy pfel'a:s k' Siomcpioarlt  MFeldaisak',,  j'ssotnaitfuys
+'i:m p'oarctt iovse
+'
+}a
+p p   =  ]F
+l a s k (
+_ _ n a mree_t_u)r
+n
+ @jaspopn.irfoyu(t{e
+ ( ' / ' ) 
+  d e f' shooumrec(e)s:'
+:   s o urrecteusr,n
+  ' ' ' 
+          '<tho1t>aBlr_ocaoducnats't:i nlge nN(eswosu rAcgegsr)e,g
+          a t o r < / h 1 >'
+a c t i v<ep_>cSoyusntte'm:  Slteant(u[ss:  fAocrt isv ei<n/ ps>o
+u r c e s< pi>f< as [h'rsetfa=t"u/sh'e]a l=t=h "'>aHcetailvteh' ]C)h
+e c k < /}a)>
+<
+/ipf> 
+_ _ n a m'e'_'_
+
+ =@=a p'p_._rmoauitne_(_''/:h
+ e a l t hp'o)r
+ td e=f  ihneta(lotsh.(e)n:v
+ i r o n .rgeettu(r'nP OjRsTo'n,i f5y0(0{0')s)t
+ a t u s 'a:p p'.hreuanl(thhoys't,= ''0s.e0r.v0i.c0e'',:  p'oBrrto=apdocrats,t idnegb uNge=wFsa lAsgeg)regator'})
+
+ if __name__ == '__main__':
+     port = int(os.environ.get('PORT', 5000))
+         app.run(host='0.0.0.0', port=port)
